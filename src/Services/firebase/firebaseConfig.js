1cbaaf2e5b932dd3eb7f2618a519dcf2874ef014
rodrigolajas <rodrigolajas18@gmail.com>
2024-01-27 18:55:20 -0300

    agregado de archivo env a git ignore

--- src/Services/firebase/firebaseConfig.js
+++ src/Services/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNpF8MxDaat1BQDrCq7eMlFYVddaHRPr8",
-    authDomain: "tienda-de-celulares-f1232.firebaseapp.com",
-    projectId: "tienda-de-celulares-f1232",
-    storageBucket: "tienda-de-celulares-f1232.appspot.com",
-    messagingSenderId: "170595257364",
-    appId: "1:170595257364:web:3d2e12291099b992fcab01"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
