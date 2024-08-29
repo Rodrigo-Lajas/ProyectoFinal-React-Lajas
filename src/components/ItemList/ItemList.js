51f3c457a7a7d8dde0fb85f0b11b3dd4df5d6e1b
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-28 23:59:11 -0300

    itemDetail

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,15 +1,3 @@
-// import Item from '../Item/Item'
-
-// const ItemList = ( { productos } ) => {
-//     return(
-//         <div className='container'>
-//             {productos.map((prod) => <Item producto={prod} key={prod.id} />)}
-//         </div>
-//     )
-// }
-
-// export default ItemList;
-
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Item from '../Item/Item';
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,3 @@
-// import CartWidget from "../CartWidget/CartWidget";
-// import { Link } from 'react-router-dom';
-
-
-// const NavBar = () => {
-//     return (
-//         <nav>
-//             <div>
-//                 <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
-//                     <h6>
-//                         Tienda de Telefonía movil
-//                     </h6>
-//                 </Link>
-
-//                 <div style={{ marginLeft: 'auto' }}>
-//                     <Link to="/category/Samsung">Samsung</Link>
-//                     <Link to="/category/Motorola">Motorola</Link>
-//                     <Link to="/category/Xiaomi">Xiaomi</Link>
-//                     <Link to="/category/ZTE">ZTE</Link>
-//                     <CartWidget />
-//                 </div>
-//             </div>
-//         </nav>
-//     )
-// }
-
-// export default NavBar;
-
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
